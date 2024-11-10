3b936bce7207378b107fa3e79797c3e88a30243c
TatLead <hkdsethomas@gmail.com>
2024-11-11 00:51:56 +0800

    Install xterm-svelte
    https://xterm-svelte.pages.dev/
    
    npm install @battlefieldduck/xterm-svelte

---
+++ src/lib/Terminal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Xterm, XtermAddon } from '@battlefieldduck/xterm-svelte';
+	import type {
+		ITerminalOptions,
+		ITerminalInitOnlyOptions,
+		Terminal
+	} from '@battlefieldduck/xterm-svelte';
+
+	let options: ITerminalOptions & ITerminalInitOnlyOptions = {
+		fontFamily: 'Consolas'
+	};
+
+	async function onLoad(terminal: Terminal) {
+		console.log('Child component has loaded');
+
+		// FitAddon Usage
+		const fitAddon = new (await XtermAddon.FitAddon()).FitAddon();
+		terminal.loadAddon(fitAddon);
+		fitAddon.fit();
+
+		terminal.write('Hello World');
+	}
+
+	function onData(data: string) {
+		console.log('onData()', data);
+	}
+
+	function onKey(data: { key: string; domEvent: KeyboardEvent }) {
+		console.log('onKey()', data);
+	}
+</script>
+
+<Xterm {options} {onLoad} {onData} {onKey} />
